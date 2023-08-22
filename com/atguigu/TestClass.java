644bfbb78f9182313685f35e2ab0d34339006dc5
lambo <lambo@atguigu.com>
2023-08-22 16:43:59 +0800

    test

--- com/atguigu/TestClass.java
+++ com/atguigu/TestClass.java
@@ -3,6 +3,9 @@ package com.atguigu;
 public class TestClass {
     public static void main(String[] args) {
         System.out.println("Test");
+        System.out.println("Test");
+        System.out.println("Test");
+        System.out.println("Test");
     }
 
 }
