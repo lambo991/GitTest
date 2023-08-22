e844ae0199483def944c5d3be4b975bd5741fa6b
zhangsan <zhangsan@atguigu.com>
2023-08-22 19:00:30 +0800

    创建独孤九剑

---
+++ com/atguigu/DuGuJiuJian.java
@@ -0,0 +1,12 @@
+package com.atguigu;
+
+public class DuGuJiuJian {
+    public static void main(String[] args) {
+        System.out.println("独孤九剑");
+        System.out.println("独孤九剑");
+        System.out.println("独孤九剑");
+        System.out.println("独孤九剑");
+        System.out.println("独孤九剑");
+        System.out.println("独孤九剑");
+    }
+}
